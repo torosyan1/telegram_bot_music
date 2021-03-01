a73a20a724c308d5d8421b372ad2571f089ac910
vahag_t <vahag@steadfast.tech>
2021-03-01 11:59:42 +0400

    test ffmpeg end

--- src/functions/dowloadeAndConvert.ts
+++ src/functions/dowloadeAndConvert.ts
@@ -39,14 +39,14 @@ export const downloadAndConvert = async (query) => {
         .saveToFile(`/app/${newName}`)
         .on("end", async () => {
           file = readFileSync(`/app/${newName}`);
-
+          console.log('chneged format')
           const params = {
             Bucket: process.env.BucketName,
             Key: newName,
             Body: file,
             ACL: "public-read",
           };
-
+        
           await S3.upload(params)
             .promise()
             .then((res) => {
